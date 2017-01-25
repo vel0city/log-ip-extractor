d5e99156f5c6f95b2b30282f25b0febef080f442
Michael Schneider <michael@simpleltc.com>
2017-01-25 14:45:45 -0600

    Initial commit
    Searches log files and finds info related to all IP addresses.

---
+++ extract.py
@@ -0,0 +1,43 @@
+import re
+import sys
+import socket
+import requests
+
+IP_REGEX = re.compile('\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b')
+IP_INFO_URL = 'http://freegeoip.net/json/{}'
+
+
+def get_ip_addrs(file):
+    ip_addrs = {}
+    lines = file.readlines()
+    sys.stdout.write("{} line file read, analyzing".format(len(lines)))
+    sys.stdout.flush()
+    for line in lines:
+        sys.stdout.write(".")
+        match = IP_REGEX.findall(line)
+        if len(match) > 0 and match[0] not in ip_addrs.keys():
+            try:
+                host = socket.gethostbyaddr(match[0])
+            except socket.herror:
+                host = ['UNKNOWN']
+            ip_addrs[match[0]] = [host[0], get_country(match[0])]
+    return ip_addrs
+
+
+def get_country(ip_addr):
+    resp = requests.get(IP_INFO_URL.format(ip_addr))
+    host_info = resp.json()
+    return host_info['country_name']
+
+
+def print_ips(ip_addrs):
+    for ip, hostname in ip_addrs.iteritems():
+        print(ip + '\t' + hostname[1] + '\t' + hostname[0])
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    with open(filename) as f:
+        ip_addrs = get_ip_addrs(f)
+    print('\n\nIP Addresses found in {}:'.format(filename))
+    print_ips(ip_addrs)
